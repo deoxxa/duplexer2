f3a44a7b9e405cd9ba7c75c30efcd0e8716ff797
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-10-17 12:34:33 +0900

    switch from Mocha to tape
    https://github.com/substack/tape

--- test.js
+++ test.js
@@ -1,149 +1,136 @@
-"use strict";
-
-const assert = require("assert");
+"use strong";
 
 const duplexer2 = require("./");
 const {Readable, Writable} = require("readable-stream");
+const {test} = require("tape");
+const through = require("through");
 
-describe("duplexer2", () => {
-  let writable;
-  let readable;
+class FixtureReadable extends Readable {
+  constructor(...args) {
+    super(Object.assign({
+      objectMode: true,
+      read: () => {}
+    }, ...args));
+  }
+}
 
-  beforeEach(() => {
-    writable = new Writable({objectMode: true});
-    readable = new Readable({objectMode: true});
+class FixtureWritable extends Writable {
+  constructor(...args) {
+    super(Object.assign({
+      objectMode: true,
+      write: (data, enc, cb) => cb()
+    }, ...args));
+  }
+}
 
-    writable._write = (data, enc, cb) => cb();
-    readable._read = () => {};
+test("duplexer2", t => {
+  t.plan(11);
+
+  duplexer2(
+    new FixtureWritable({
+      write(data, enc, cb) {
+        t.strictEqual(
+          data.toString(),
+          "Hi",
+          "should interact with the writable stream properly for writing"
+        );
+        cb();
+      }
+    }),
+    new FixtureReadable()
+  ).write("Hi");
+
+  const duplexStream = duplexer2(new FixtureWritable(), new FixtureReadable())
+  .once("data", data => {
+    t.strictEqual(
+      data.toString(),
+      "H",
+      "should interact with the readable stream properly for reading"
+    );
   });
 
-  it("should interact with the writable stream properly for writing", done => {
-    writable._write = (data, enc, cb) => {
-      assert.strictEqual(data.toString(), "well hello there");
-      done();
-      cb();
-    };
-
-    duplexer2(writable, readable).write("well hello there");
-  });
-
-  it("should interact with the readable stream properly for reading", done => {
-    duplexer2(writable, readable)
-    .on("data", data => {
-      assert.strictEqual(data.toString(), "well hello there");
-      done();
+  duplexStream._readable.push("H");
+  process.nextTick(() => {
+    duplexStream.once("data", data => {
+      t.strictEqual(
+        data.toString(),
+        "ello",
+        "should read data multiple times"
+      );
     });
-
-    readable.push("well hello there");
+    duplexStream._readable.push("ello");
   });
 
-  it("should end the writable stream, causing it to finish", done => {
-    writable.once("finish", done);
-    duplexer2(writable, readable).end();
-  });
+  duplexer2(new FixtureWritable().once("finish", () => {
+    t.pass("should end the writable stream, causing it to finish");
+  }), new FixtureReadable()).end();
 
-  it("should finish when the writable stream finishes", done => {
-    duplexer2(writable, readable).once("finish", done);
-    writable.end();
-  });
+  duplexer2(new FixtureWritable(), new FixtureReadable())
+  .once("finish", () => t.pass("should finish when the writable stream finishes"))
+  ._writable.end();
 
-  it("should end when the readable stream ends", done => {
-    duplexer2(writable, readable)
-    .once("end", done)
-    .resume(); // required to let "end" fire without reading
+  duplexer2(new FixtureWritable(), new FixtureReadable())
+  .once("end", () => t.pass("should end when the readable stream ends"))
+  .resume() // required to let "end" fire without reading
+  ._readable.push(null);
 
-    readable.push(null);
-  });
+  const originalWritableErr = Error("testing");
 
-  it("should bubble errors from the writable stream when no behaviour is specified", done => {
-    const originalErr = Error("testing");
-
-    duplexer2(writable, readable)
-    .on("error", err => {
-      assert.strictEqual(err, originalErr);
-      done();
-    });
-
-    writable.emit("error", originalErr);
-  });
-
-  it("should bubble errors from the readable stream when no behaviour is specified", done => {
-    const originalErr = Error("testing");
-
-    duplexer2(writable, readable)
-    .on("error", err => {
-      assert.strictEqual(err, originalErr);
-      done();
-    });
-
-    readable.emit("error", originalErr);
-  });
-
-  it("should bubble errors from the writable stream when bubbleErrors is true", done => {
-    const originalErr = Error("testing");
-
-    duplexer2({bubbleErrors: true}, writable, readable)
-    .on("error", err => {
-      assert.strictEqual(err, originalErr);
-      done();
-    });
-
-    writable.emit("error", originalErr);
-  });
-
-  it("should bubble errors from the readable stream when bubbleErrors is true", function(done) {
-    const originalErr = Error("testing");
-
-    duplexer2({bubbleErrors: true}, writable, readable)
-    .on("error", function(err) {
-      assert.strictEqual(err, originalErr);
-      done();
-    });
-
-    readable.emit("error", originalErr);
-  });
-
-  it("should not bubble errors from the writable stream when bubbleErrors is false", done => {
-    const timeout = setTimeout(done, 25);
-
-    duplexer2({bubbleErrors: false}, writable, readable)
-    .on("error", err => {
-      clearTimeout(timeout);
-      done(Error("shouldn't bubble error"));
-    });
-
-    writable
-    .on("error", () => {}) // prevent uncaught error exception
-    .emit("error", Error("testing"));
-  });
-
-  it("should not bubble errors from the readable stream when bubbleErrors is false", done => {
-    const timeout = setTimeout(done, 25);
-
-    duplexer2({bubbleErrors: false}, writable, readable)
-    .on("error", err => {
-      clearTimeout(timeout);
-      done(Error("shouldn't bubble error"));
-    });
-
-    readable
-    .on("error", () => {}) // prevent uncaught error exception
-    .emit("error", Error("testing"));
-  });
-
-  it("should work with streams1", function(done) {
-    const readable1 = new Readable();
-    readable1.readable = true;
-    readable1._read = () => {};
-
-    duplexer2(writable, readable1)
-    .on("readable", function() {
-      assert.strictEqual(this.read().toString(), "well hello there");
-      done();
-    });
-
-    // making sure we're on compat mode.
-    readable1.on('end', function() {})
-    readable1.push("well hello there");
+  duplexer2(new FixtureWritable(), new FixtureReadable())
+  .on("error", err => {
+    t.strictEqual(
+      err,
+      originalWritableErr,
+      "should bubble errors from the writable stream"
+    );
   })
+  ._writable.emit("error", originalWritableErr);
+
+  const originalReadableErr = Error("testing");
+
+  duplexer2(new FixtureWritable(), new FixtureReadable())
+  .on("error", err => {
+    t.strictEqual(
+      err,
+      originalReadableErr,
+      "should bubble errors from the readable stream"
+    );
+  })
+  ._readable.emit("error", originalReadableErr);
+
+  const timeout = setTimeout(() => {
+    t.pass("should not bubble errors from the streams when bubbleErrors is false");
+  }, 25);
+
+  const writable = new FixtureWritable();
+  const readable = new FixtureReadable();
+
+  duplexer2({bubbleErrors: false}, writable, readable)
+  .on("error", () => {
+    clearTimeout(timeout);
+    t.fail("shouldn't bubble error");
+  });
+
+  writable
+  .on("error", () => {}) // prevent uncaught error exception
+  .emit("error", Error("testing"));
+
+  readable
+  .on("error", () => {}) // prevent uncaught error exception
+  .emit("error", Error("testing"));
+
+  const stream1 = through();
+
+  duplexer2(null, new FixtureWritable(), stream1)
+  .on("readable", function() {
+    t.strictEqual(this.read().toString(), "well hello there", "should work with streams1");
+  });
+
+  stream1.push("well hello there");
+
+  t.throws(
+    () => duplexer2({bubbleErrors: 1}, new Writable(), new Readable()),
+    /TypeError.*1 is not a Boolean value\./,
+    "should throw a type error when it takes `bubbleError` option but it's not Boolean"
+  );
 });
